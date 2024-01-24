1bdda756348b4ecf24a1282479c404c82de4b714
akbar5yn <akabr5yn.developer@gmail.com>
2024-01-24 08:22:36 +0700

    membuat halaman splash screen

--- app/src/main/java/com/example/myapp/SplashScreen.kt
+++ app/src/main/java/com/example/myapp/SplashScreen.kt
@@ -1,2 +1,105 @@
 package com.example.myapp
 
+import android.window.SplashScreen
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.myapp.ui.theme.MyAppTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    val alpha = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        alpha.animateTo(
+            1f,
+            animationSpec = tween(5000)
+        )
+        delay(5000)
+        navController.popBackStack()
+        navController.navigate("Home")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(if (isSystemInDarkTheme()) Color.Black else Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val myFont = FontFamily(
+            Font(R.font.montserratalternates_regular, FontWeight.Medium)
+        )
+        LoaderAnimation(
+            modifier = Modifier
+                .size(400.dp),
+            anim = R.raw.mobile_app
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Text(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 0.dp, end = 20.dp, bottom = 0.dp)
+                .alpha(alpha.value),
+            text = "Please wait a second until your app starts",
+            fontFamily = myFont,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Light,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun LoaderAnimation(modifier: Modifier, anim: Int) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(anim))
+    LottieAnimation(
+        composition = composition, iterations = LottieConstants.IterateForever,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    val navController = rememberNavController()
+    MyAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = if (isSystemInDarkTheme()) Color.Black else Color.White
+        ) {
+            SplashScreen(navController)
+        }
+    }
+}
